de61eb912515727c1cd744a720a550e9fb6a3b5e
Animesh-weavers <animesh.dey@weavers-web.com>
2022-04-12 11:53:38 +0530

    increase loader container height

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -3,7 +3,7 @@ import { ThreeCircles } from "react-loader-spinner";
 
 const LoaderWb = () => {
   return (
-    <div style={{ width: "100%", height: "90vh",display:'flex',justifyContent:'center',alignItems:'center' }}>
+    <div style={{ width: "100%", height: "98vh",display:'flex',justifyContent:'center',alignItems:'center' }}>
       <ThreeCircles
         color="grey"
         height={110}
